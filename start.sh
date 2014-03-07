7634bc6e8b1fe56682955f5343cd9a883b1d71f4
Adam Burmister <adam.burmister@xero.com>
2014-03-07 15:00:04 -0800

    Adding start up script

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
+then
+        export PATH=/usr/local/bin:$PATH
+        forever start --sourceDir /srv/gitprint.com/ server.js >> /var/log/gitprint.com.txt 2>&1
+fi
