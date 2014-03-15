3f22d14f7419644473243a98724454fd74f11e9f
Adam Burmister <adam.burmister@gmail.com>
2014-03-14 18:09:12 -0700

    Compiled

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,51 +2,14 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    // concat: {
-    //   options: {
-    //     separator: ';'
-    //   },
-    //   dist: {
-    //     src: ['src/**/*.js'],
-    //     dest: 'dist/<%= pkg.name %>.js'
-    //   }
-    // },
-    // uglify: {
-    //   options: {
-    //     banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-    //   },
-    //   dist: {
-    //     files: {
-    //       'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-    //     }
-    //   }
-    // },
-    // qunit: {
-    //   files: ['test/**/*.html']
-    // },
-    // jshint: {
-    //   files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-    //   options: {
-    //     // options here to override JSHint defaults
-    //     globals: {
-    //       jQuery: true,
-    //       console: true,
-    //       module: true,
-    //       document: true
-    //     }
-    //   }
-    // },
     sass: {
       site: {
         options: {
           style: 'expanded'
         },
-        files: [{
-          expand: true,
-          cwd: 'public/scss/',
-          src: '{style,printView}.scss',
-          dest: 'public/css/site.css',
-        }]
+        files: {
+          'public/css/main.css': 'public/scss/main.scss',
+        }
       },
       print: {
         options: {
