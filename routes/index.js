9f26e31d84b281ac970d9f56e0a179a117fdd2d1
Adam Burmister <adam.burmister@gmail.com>
2014-04-12 11:02:09 -0700

    Needless work since I use etags already, but playing with the idea

--- routes/index.js
+++ routes/index.js
@@ -39,70 +39,80 @@ var DEFAULT_DISPOSITION = DISPOSITION.INLINE;
  * @param {object} response
  * @param {string} url
  * @param {string} disposition ('inline' | 'attachment')
+ * @param {boolean} cachebust Skip serving rendered file from disk cache
  * @param {function} pre-process markdown
  * @param {function} pre-process html
  */
-function convert(req, res, url, disposition, preProcessMd, preProcessHtml) {
-  var requestOptions = {
-    method: 'HEAD',
-    uri: url,
-    followAllRedirects: true
-  };
+function convert(req, res, url, disposition, cachebust, preProcessMd, preProcessHtml) {
+  _isCached(url, function(err, cachePath) {
+    var exists = !!cachePath;
+    var pdfFilename = 'gitprint__' + (req.path.replace(/[^a-zA-Z0-9-_\.]/gi,'-')).substr(0, MAX_FILENAME_LEN) + '.pdf';
+    var headerContentDisposition = (disposition || DEFAULT_DISPOSITION) + '; filename="' + pdfFilename + '"';
+    
+    res.setHeader('Content-disposition', headerContentDisposition);
+    res.contentType('application/pdf');
 
-  // Do a cheaper HEAD request first to see if we already have a pre-rendered PDF on disk cache
-  request(requestOptions, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      // The HEAD request worked, no check the cache for a PDF
+    if (exists && !cachebust) {
+      // We have a copy already! Just send that
+      console.log('Serving PDF from cache, ' + cachePath);
+      fs.createReadStream(cachePath).pipe(res);
+    } else {
+      // We don't have a prerendered PDF, so download and render one
+      // Upgrade to a full GET request...
+      requestOptions.method = 'GET';
+      request(requestOptions, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var options = _.extend({}, MARKDOWN_OPTIONS);
+          // Extend md options with any processors
+          if(preProcessMd) {
+            options.preProcessMd = preProcessMd;
+          }
+          if(preProcessHtml) {
+            options.preProcessHtml = preProcessHtml;
+          }
 
-      // Where are we saving this file?
-      var hash = crypto.createHash('md5').update(url).digest('hex'); // fingerprint path
-      var etag = (response.headers.etag || 'no_etag').replace(/"/g,''); // get the etag (stripping quotes)
-      var outputPath = __dirname + '/../cache/' + hash + '-' + etag + '.pdf';
-
-      fs.exists(outputPath, function(exists) {
-        var pdfFilename = 'gitprint__' + (req.path.replace(/[^a-zA-Z0-9-_\.]/gi,'-')).substr(0, MAX_FILENAME_LEN) + '.pdf';
-        var headerContentDisposition = (disposition || DEFAULT_DISPOSITION) + '; filename="' + pdfFilename + '"';
-        
-        res.setHeader('Content-disposition', headerContentDisposition);
-        res.contentType('application/pdf');
-
-        if (exists) {
-          // We have a copy already! Just send that
-          console.log('Serving PDF from cache, ' + outputPath);
-          fs.createReadStream(outputPath).pipe(res);
-        } else {
-          // We don't have a prerendered PDF, so download and render one
-          // Upgrade to a full GET request...
-          requestOptions.method = 'GET';
-          request(requestOptions, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-              var options = _.extend({}, MARKDOWN_OPTIONS);
-              // Extend md options with any processors
-              if(preProcessMd) {
-                options.preProcessMd = preProcessMd;
-              }
-              if(preProcessHtml) {
-                options.preProcessHtml = preProcessHtml;
-              }
-
-              markdownpdf(options).from.string(body).to(outputPath, function (data) {
-                var stream = fs.createReadStream(outputPath);                
-                stream.pipe(res);
-              });
-            } else {
-              res.contentType('text/html');
-              res.send(500, 'Something went wrong! Couldn\'t process ' + url);
-            }
+          markdownpdf(options).from.string(body).to(cachePath, function (data) {
+            var stream = fs.createReadStream(cachePath);                
+            stream.pipe(res);
           });
-
+        } else {
+          res.contentType('text/html');
+          res.send(500, 'Something went wrong! Couldn\'t process ' + url);
         }
       });
 
-    } else {
-      // The HEAD failed
-      res.send(500, 'Something went wrong! Couldn\'t process ' + url); 
     }
 
+  });
+}
+
+function _getCachePath(githubUrl, response) {
+  var hash = crypto.createHash('md5').update(githubUrl).digest('hex'); // fingerprint path
+  var etag = (response.headers.etag || 'no_etag').replace(/"/g,''); // get the etag (stripping quotes)
+  return __dirname + '/../cache/' + hash + '-' + etag + '.pdf';
+}
+
+/** 
+ * Do a cheaper HEAD request to see if we already have a pre-rendered PDF on disk cache
+ */
+function _isCached(githubUrl, cb) {
+  var requestOptions = {
+    method: 'HEAD',
+    uri: githubUrl,
+    followAllRedirects: true
+  };
+
+  request(requestOptions, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // The HEAD request worked
+      var cachePath = _getCachePath(githubUrl, response);
+      fs.exists(cachePath, function(exists) {
+        cb(null, cachePath);
+      });
+    } else {
+      // The HEAD failed
+      cb("Couldn't process " + githubUrl, null);
+    }
   });
 }
 
@@ -127,64 +137,75 @@ function _getMarkdownPreProcessors(url) {
   return pproc;
 }
 
-exports.convertGistMarkdownToPdf = function(req, res) {
-  console.log("convertGistMarkdownToPdf");
+// exports.convertGistMarkdownToPdf = function(req, res) {
+//   console.log("convertGistMarkdownToPdf");
 
-  // // https://gist.githubusercontent.com
-  // var githubPath = '';
-  // 'https://gist.githubusercontent.com/' + githubPath;
+//   // // https://gist.githubusercontent.com
+//   // var githubPath = '';
+//   // 'https://gist.githubusercontent.com/' + githubPath;
   
-  // if(Object.keys(req.query).indexOf('download') !== -1) {
-  //   convert(req, res, url, DISPOSITION.ATTACHMENT);
-  // } else if(Object.keys(req.query).indexOf('inline') !== -1) {
-  //   convert(req, res, url, DISPOSITION.INLINE);
-  // } else {
-  //   res.render('printView', { pageTitle: githubPath });
-  // }
-};
+//   // if(Object.keys(req.query).indexOf('download') !== -1) {
+//   //   convert(req, res, url, DISPOSITION.ATTACHMENT);
+//   // } else if(Object.keys(req.query).indexOf('inline') !== -1) {
+//   //   convert(req, res, url, DISPOSITION.INLINE);
+//   // } else {
+//   //   res.render('printView', { pageTitle: githubPath });
+//   // }
+// };
 
-exports.convertWikiMarkdownToPdf = function(req, res){
+// exports.convertWikiMarkdownToPdf = function(req, res){
+//   var githubPath = req.path;
+//   var url = urlHelper.translate(githubPath);
+
+//   var isDownload = Object.keys(req.query).indexOf('download') !== -1;
+//   var isInline = Object.keys(req.query).indexOf('inline') !== -1;
+//   var cachebust = Object.keys(req.query).indexOf('cachebust') !== -1;
+
+//   if(isDownload) {
+//     convert(req, res, url, DISPOSITION.ATTACHMENT, cachebust, _getMarkdownPreProcessors(url));
+//   } else if(isInline) {
+//     convert(req, res, url, DISPOSITION.INLINE, cachebust, _getMarkdownPreProcessors(url));
+//   } else {
+//     res.render('printView', { pageTitle: githubPath });
+//   }
+// };
+
+
+// exports.convertMarkdownToPdf = function(req, res){
+//   var githubPath = req.path;
+//   var url = urlHelper.translate(githubPath);
+
+//   var isDownload = Object.keys(req.query).indexOf('download') !== -1;
+//   var isInline = Object.keys(req.query).indexOf('inline') !== -1;
+//   var cachebust = Object.keys(req.query).indexOf('cachebust') !== -1;
+
+//   if(isDownload || isInline) {
+//     var disposition = (isDownload ? DISPOSITION.ATTACHMENT : DISPOSITION.INLINE);
+//     convert(req, res, url, disposition, cachebust, _getMarkdownPreProcessors(url));
+//   } else {
+//     res.render('printView', { pageTitle: githubPath });
+//   }
+// };
+
+exports.convert = function(req, res){
   var githubPath = req.path;
-  var url = urlHelper.translate(githubPath);
+  var paramKeys = Object.keys(req.query);
+  var isDownload = paramKeys.indexOf('download') !== -1;
+  var isInline = paramKeys.indexOf('inline') !== -1;
+  var cachebust = !!(req.query['cache']) && paramKeys.indexOf('cache') !== -1;
 
-  if(Object.keys(req.query).indexOf('download') !== -1) {
-    convert(req, res, url, DISPOSITION.ATTACHMENT, _getMarkdownPreProcessors(url));
-  } else if(Object.keys(req.query).indexOf('inline') !== -1) {
-    convert(req, res, url, DISPOSITION.INLINE, _getMarkdownPreProcessors(url));
-  } else {
-    res.render('printView', { pageTitle: githubPath });
-  }
-};
-
-
-exports.convertMarkdownToPdf = function(req, res){
-  var githubPath = req.path;
-  var url = urlHelper.translate(githubPath);
-
-  if(Object.keys(req.query).indexOf('download') !== -1) {
-    convert(req, res, url, DISPOSITION.ATTACHMENT, _getMarkdownPreProcessors(url));
-  } else if(Object.keys(req.query).indexOf('inline') !== -1) {
-    convert(req, res, url, DISPOSITION.INLINE, _getMarkdownPreProcessors(url));
-  } else {
-    res.render('printView', { pageTitle: githubPath });
-  }
-};
-
-exports.convertRopoIndexMarkdownToPdf = function(req, res){
-  var githubPath = req.path;
-  var url;
-  var disposition = DISPOSITION.INLINE;
-
-  if(Object.keys(req.query).indexOf('download') !== -1 || Object.keys(req.query).indexOf('inline') !== -1) {
-    if(Object.keys(req.query).indexOf('download') !== -1) {
-      disposition = DISPOSITION.ATTACHMENT;
+  Q.when(urlHelper.translate(githubPath)).then(function(url) {
+    if(isDownload || isInline) {
+      var disposition = (isDownload ? DISPOSITION.ATTACHMENT : DISPOSITION.INLINE);
+      convert(req, res, url, disposition, cachebust, _getMarkdownPreProcessors(url));
+    } else {
+      _isCached(url, function(err, cachePath) {
+        var exists = !!cachePath;
+        var servedFromCache = exists && !cachebust;
+        res.render('printView', { pageTitle: githubPath, servedFromCache: servedFromCache });
+      });
     }
-    Q.when(urlHelper.translate(githubPath)).then(function(url) {
-      convert(req, res, url, disposition, _getMarkdownPreProcessors(url));
-    });
-  } else {
-    res.render('printView', { pageTitle: githubPath });
-  }
+  });
 };
 
 /* GET home page. */
